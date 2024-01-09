f64d42a3be89d4722ee577a1b76ed159e1940ab2
sugarsugar12 <146008738+sugarsugar12@users.noreply.github.com>
2024-01-09 21:51:03 +0900

    third commit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -99,7 +99,7 @@ RSpec.describe User, type: :model do
       end
 
       it 'passwordが6文字以上でないと登録できない' do
-        @user.password = 'aaaaa'
+        @user.password = 'aaaa1'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
       end
