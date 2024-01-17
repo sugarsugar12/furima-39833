333f8a9db0a284420b6e1f74c7d8aca7eae809b4
sugarsugar12 <146008738+sugarsugar12@users.noreply.github.com>
2024-01-17 17:49:58 +0900

    first commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :new,:create,:show,:update,:destroy]
+
 
   def index
     @items=Item.all.order(created_at: :desc)
@@ -23,9 +25,29 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+       redirect_to item_path(@item.id)
+       #詳細ページ
+    else
+       render :edit, status: :unprocessable_entity
+    end
+  end
 
  private
   def item_params
     params.require(:item).permit(:title,:explanation,:category_id,:quality_id,:delivery_charge_id,:source_id,:number_of_day_id,:price,:image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
